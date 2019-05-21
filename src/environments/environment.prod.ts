3405da818959b3736b589d91096100f80f557f54
adriangarciag <ad_j_gg@hotmail.es>
2019-05-21 16:26:08 -0500

    Primer commitee

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+   firebase: {
+   /* apiKey: "AIzaSyBMOaRr0NirNOX-eh2ITJBHUpL3ow8Hr4w",
+    authDomain: "splendid-ground-95214.firebaseapp.com",
+    databaseURL: "https://splendid-ground-95214.firebaseio.com",
+    projectId: "splendid-ground-95214",
+    storageBucket: "",
+    messagingSenderId: "429226112331"*/
+    apiKey: "AIzaSyC7NuGaOIll7llCM8l8eWmPUI1CXhtQxfI",
+    authDomain: "appangular-firebase.firebaseapp.com",
+    databaseURL: "https://appangular-firebase.firebaseio.com",
+    projectId: "appangular-firebase",
+    storageBucket: "appangular-firebase.appspot.com",
+    messagingSenderId: "97441666927",
+    appId: "1:97441666927:web:4f3137eb1ea79e86"
+  }
 };
